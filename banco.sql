fb281aa6f8952d07946d27d702bd420352851e17
manoelavm <manozinha90@gmail.com>
2026-08-05 23:05:58 -0300

    Cambios

--- banco.sql
+++ banco.sql
@@ -42,3 +42,27 @@ CREATE TABLE comentarios (
     datacomentario TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- também serve para colocar automático a data
     FOREIGN KEY (usuarioId) REFERENCES usuarios(idUsuario) ON DELETE CASCADE
 );
+
+-- Inseri alguns dados para testar o funcionamento do nosso banco
+
+INSERT INTO usuarios (nome, telefone, email, senha, tipoDeUsuario) VALUES
+('Valmira Valente dos Reis','55984672390','dosreisvalmira@hotel.com', '87654321', 'dono'),
+('Júlio Castiel da Silva','55984549087','juliocast@hotel.com', '87654321', 'funcionário'),
+('Maria Ferreira Machado','559817393567','mferreiramaria@hotel.com', '12345678', 'hóspede');
+
+INSERT INTO catservicos (nome) VALUES
+('Piscina e spa'),
+('Academia'),
+('Restaurante'),
+('Sala de Eventos');
+
+INSERT INTO servicos (nome, descricao, idCategoria, statusDoServico, motivo, atualizadoPor) VALUES
+('Piscina climatizada', 'Piscina interior com horário das 08h00 às 22h00', 1, 'disponível', NULL, 2),
+('Spa', 'Serviço de Spa com reserva prévia', 1, 'manutenção', 'Reparação no sistema de calefação', 2),
+('Academia SuperFit', 'Estrutura com todos os itens necessários para sua rotina de exercícios', 2, 'disponível', NULL, 2),
+('Restaurante Coisa Boa', 'Possui todas as três refeições além de uma vista para o jardim', 3, 'disponível', NULL, 2),
+('Sala de Eventos Pandora', 'Têm limite para até 80 pessoas', 4, 'fechado', 'O salão está interditado para limpeza', 2);
+
+INSERT INTO comentarios (usuarioId, conteudo) VALUES
+(1, 'Sejam bem-vindos ao fórum do Hotel Fronteira! Por favor, mantenham respeito e respeitem as normas do hotel.'),
+(3, 'Oii, por acaso alguém sabe me dizer se o a piscina fica aberta ao domingos?');
